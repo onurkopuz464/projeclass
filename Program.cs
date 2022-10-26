0508c60c9c4f06da2ea50374dbeaa30de6e9aa0b
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-26 17:08:31 +0300

    q

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Works works = new Works();
+            Works worksTwo = new Works();
+            works.name = "ali";works.surname = "pehlivan"; works.country = "izmir";
+            worksTwo.name = "onur"; worksTwo.surname = "kopuz"; worksTwo.country = "izmir";
+            works.info();worksTwo.info();
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+    class Works
+    {
+        public string name;
+        public string surname;
+        public string country;
+
+        public void info()
+        {
+            Console.Write(name + " " + surname + " " + country);
+
+
+
+        }
+
+
+        
+    }
+    
+}
+
